17d18339bc9a313cb278a647a8c502e44d73d2b4
kooogi <kogiplayinggames@gmail.com>
2026-07-09 19:20:38 +0200

    Remove deprecated files including headers, .htaccess, example config, light theme redirect, and README; streamline project structure.

--- config.example.js
+++
@@ -1,3 +0,0 @@
-window.SITE_CONFIG = Object.freeze({
-  web3formsAccessKey: 'YOUR_WEB3FORMS_ACCESS_KEY'
-});
--- redirect-light.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-  try {
-    localStorage.setItem('theme', 'light');
-  } catch (e) {}
-  location.replace('index.html');
-})();
